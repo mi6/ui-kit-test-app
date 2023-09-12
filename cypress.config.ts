7043d5773869d4976e22cb073bc10ff80bc4d371
mmb675 <mmb675@proton.me>
2023-09-12 08:28:37 +0000

    update reporting file and add some import files

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "cypress";
 
+
+
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',//for html report
+// reporter: 'reporters/custom.js',
+
+
+  
+  
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      //require('cypress-mochawesome-reporter/plugin')(on);//for html report
+      
     },
     baseUrl: 'http://localhost:5173/',
   },
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -16,6 +16,6 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import 'cypress-axe'
-
+//import 'cypress-mochawesome-reporter/register';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
