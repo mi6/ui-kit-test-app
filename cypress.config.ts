b3b7af2903be2ba0c7f4eeb0384781529046f501
jd239 <116016939+jd239@users.noreply.github.com>
2023-09-13 11:37:44 +0000

    Cypress reporting fix

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,19 +1,12 @@
 import { defineConfig } from "cypress";
 
-
-
 export default defineConfig({
-  reporter: 'cypress-mochawesome-reporter',//for html report
+  reporter: 'cypress-mochawesome-reporter',
 // reporter: 'reporters/custom.js',
-
-
-  
-  
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      //require('cypress-mochawesome-reporter/plugin')(on);//for html report
-      
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl: 'http://localhost:5173/',
   },
